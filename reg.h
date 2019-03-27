64dc6e2a70e5dc04e3c1a21319cb71c18f35fa79
ccc0936 <clt0592xm@gmail.com>
2019-03-27 14:49:05 +0800

    al

--- reg.h
+++ reg.h
@@ -9,8 +9,7 @@
 
 #define SET_BIT(addr, bit) (REG(addr) |= UINT32_1 << (bit))
 #define CLEAR_BIT(addr, bit) (REG(addr) &= ~(UINT32_1 << (bit)))
-
-// #define READ_BIT(addr, bit) ??????
+#define read_bit(addr,bit) (REG(addr) &= UINT32_1<<(bit))
 
 //RCC
 #define RCC_BASE 0x40023800
@@ -45,4 +44,6 @@
 #define BRy_BIT(y) ((y) + 16)
 #define BSy_BIT(y) (y)
 
+#define GPIOx_IDR_OFFSET 0x10 
+#define GPIOx_IDR_BIT(y) (y)
 #endif
